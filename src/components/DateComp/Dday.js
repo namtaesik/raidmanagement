f4389b7d87841acc5d718b97782d803c907c9c08
tsnam <tsnam@broadcns.com>
2023-04-03 11:30:42 +0900

    > 일정 신청 후 인원수 바로반영되도록 변경 > d-day 제대로표시되게 수정 > d-day 1일 미만일 경우 남은 시간 표기하도록 변경

--- src/components/DateComp/Dday.js
+++ src/components/DateComp/Dday.js
@@ -1,12 +1,26 @@
 import React, { useState } from "react";
 import Moment from "moment";
 const getDaysUntilEvent = (eventDate) => {
+  const today = Moment().format("YYYY-MM-DD");
+  const eventDay = Moment(eventDate).format("YYYY-MM-DD");
+  const daysUntilEvent = Moment(eventDay).diff(today, "days");
+  return daysUntilEvent;
+};
+const gethoursUntilEvent = (eventDate) => {
   const today = Moment();
-  const daysUntilEvent = Moment(eventDate).diff(today, "days");
+  const daysUntilEvent = Moment(eventDate).diff(today, "hours");
   return daysUntilEvent;
 };
 export default function Dday(props) {
-  const diffDay = getDaysUntilEvent(props.eventDate);
+  var diffText = "";
+  var diffDay = getDaysUntilEvent(props.eventDate);
+  if (diffDay == 0) {
+    diffDay = gethoursUntilEvent(props.eventDate);
+    diffText = diffDay + "시간 전";
+  } else {
+    diffText = diffDay > 0 ? "D-" + diffDay : "D+" + diffDay;
+  }
+
   return (
     <p
       style={{
@@ -18,7 +32,7 @@ export default function Dday(props) {
         ...props.sx,
       }}
     >
-      {diffDay > 0 ? "D-" + diffDay : "D+" + diffDay}
+      {diffText}
     </p>
   );
 }
