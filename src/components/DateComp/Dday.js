1738f72de6ebb59843fb656a1d93fc8efeca3d69
tsnam <tsnam@broadcns.com>
2023-05-09 13:10:52 +0900

    일정등록 팝업 버그 수정, Dday 분단위추가, Dday 종료 표시 추가

--- src/components/DateComp/Dday.js
+++ src/components/DateComp/Dday.js
@@ -11,19 +11,48 @@ const gethoursUntilEvent = (eventDate) => {
   const daysUntilEvent = Moment(eventDate).diff(today, "hours");
   return daysUntilEvent;
 };
+const getMinuteUntilEvent = (eventDate) => {
+  const today = Moment();
+  const daysUntilEvent = Moment(eventDate).diff(today, "minute");
+  return daysUntilEvent;
+};
+
 export default function Dday(props) {
+  var backGroundColor = "#CAE3FF"; // 기본 푸른계열 색
+  var fontColor = "black";
   var diffText = "";
   var diffDay = getDaysUntilEvent(props.eventDate);
-  if (diffDay == 0) {
-    diffDay = gethoursUntilEvent(props.eventDate);
-    diffText = diffDay + "시간 전";
-  } else {
+  if (diffDay > 0) {
+    //일자표시
     diffText = diffDay > 0 ? "D-" + diffDay : "D+" + diffDay;
+  } else {
+    // 시간표시
+    var diffHour = gethoursUntilEvent(props.eventDate);
+    if (diffHour > 0) {
+      backGroundColor = "orange";
+      diffText = diffHour + "시간 전";
+    } else {
+      var diffMinute = getMinuteUntilEvent(props.eventDate);
+      // 분 표시
+      if (diffMinute > 0) {
+        backGroundColor = "red";
+        fontColor = "white";
+        diffText = diffMinute + "분 전";
+      }
+      //종료표시
+      else {
+        backGroundColor = "gray";
+        fontColor = "white";
+        diffText = "종료";
+      }
+    }
   }
 
   return (
     <p
       style={{
+        background: backGroundColor,
+        color: fontColor,
         borderRadius: "5px",
         fontSize: "12px",
         paddingLeft: "10px",
