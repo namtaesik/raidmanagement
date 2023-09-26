d4859808f69c0ae71ca017e8022a3055f96a7041
tsnam <skaxotlrq@gmail.com>
2023-09-26 18:32:52 +0900

    레이드 수정 시 텍스트필드 직접수정 막음(직접 수정시 onError발생. invalidvalue 뜸..)

--- src/components/Popup/RaidV2/DatePickerPopupV2.js
+++ src/components/Popup/RaidV2/DatePickerPopupV2.js
@@ -12,7 +12,15 @@ import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import CloseIcon from "@mui/icons-material/Close";
 import Slide from "@mui/material/Slide";
-import { Fab, FormControlLabel, MenuItem, Select, SpeedDialIcon, Switch, TextField } from "@mui/material";
+import {
+  Fab,
+  FormControlLabel,
+  MenuItem,
+  Select,
+  SpeedDialIcon,
+  Switch,
+  TextField,
+} from "@mui/material";
 import store from "../../../store";
 import { apiAxiosPromise } from "../../../services/apiAxios/apiAxios";
 // 캘린더
@@ -48,7 +56,9 @@ export default function AddJobOffer(props) {
   // 로그인 한 유저 ID 조회
   var userId = store.getState().loginUser.userId;
   // 날짜선택 값
-  const [datePickerValue, setDatePickerValue] = useState(dayjs().format("YYYY/MM/DD 19:00"));
+  const [datePickerValue, setDatePickerValue] = useState(
+    dayjs().format("YYYY/MM/DD 19:00")
+  );
   React.useEffect(() => {
     // 캐릭터 정보 조회하기
     apiAxiosPromise("GET", "/api/character", {
@@ -86,7 +96,9 @@ export default function AddJobOffer(props) {
       .then((res) => {
         res = res.filter((item) => item.code != "");
         setContentsCodeList(res);
-        setContentsCode(props.contentsCode == "" ? "Other" : props.contentsCode);
+        setContentsCode(
+          props.contentsCode == "" ? "Other" : props.contentsCode
+        );
       })
       .catch((err) => {
         console.log(err);
@@ -134,7 +146,10 @@ export default function AddJobOffer(props) {
       alert("최대인원을 1명 이상으로 설정해주세요.");
       return false;
     }
-    const ment = props.attackId == undefined ? "레이드 일정을 등록하시겠습니까?" : "레이드 일정을 수정하시겠습니까?";
+    const ment =
+      props.attackId == undefined
+        ? "레이드 일정을 등록하시겠습니까?"
+        : "레이드 일정을 수정하시겠습니까?";
     if (window.confirm(ment)) {
       var saveObj = {
         attackId: props.attackId,
@@ -183,179 +198,230 @@ export default function AddJobOffer(props) {
 
   return (
     <div>
-      <Dialog fullScreen open={props.open} onClose={handleClose} TransitionComponent={Transition}>
+      <Dialog
+        fullScreen
+        open={props.open}
+        onClose={handleClose}
+        TransitionComponent={Transition}
+      >
         <AppBar sx={{ position: "relative" }}>
           <Toolbar>
-            <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={handleClose}
+              aria-label="close"
+            >
               <CloseIcon />
             </IconButton>
             <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
-              {props.attackId == undefined ? "레이드 일정 추가" : "레이드 일정 수정"}
+              {props.attackId == undefined
+                ? "레이드 일정 추가"
+                : "레이드 일정 수정"}
             </Typography>
-            
           </Toolbar>
         </AppBar>
-        <Container maxWidth="sm" sx={{ justifyContent: "center" , marginBottom:'100px', paddingLeft:'2px',paddingRight:'2px'}}>
-        <List>
-          <ListItem key="typoTitle" >
-            <Typography variant="body2" color="text.secondary" sx={{ width: "100%", maxWidth:'800px' }}>
-              파티제목
-            </Typography>
-            
-          </ListItem>
-          <ListItem key="inputTitle">
-            <TextField
-              id="title"
-              label="제목"
-              variant="outlined"
-              value={title}
-              autoFocus
-              onChange={(evt) => {
-                if (evt.target.value.length > 39) {
-                  alert("40자까지만 입력하세요.");
-                }
-                setTitle(evt.target.value);
-              }}
-            />
-          </ListItem>
-          <Divider />
-          <ListItem key="typoContentAndDifficulty">
-            <Typography variant="body2" color="text.secondary">
-              컨텐츠 및 난이도 선택
-            </Typography>
-          </ListItem>
-          <ListItem key="selectContentAndDifficulty">
-            <Stack direction="row" justifyContent="start" alignItems="start" spacing={0} sx={{ width: "100%" }}>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="컨텐츠"
-                onChange={(evt) => {
-                  setContentsCode(evt.target.value);
-                }}
-                value={contentsCode}
-                sx={{ width: "50%" }}
+        <Container
+          maxWidth="sm"
+          sx={{
+            justifyContent: "center",
+            marginBottom: "100px",
+            paddingLeft: "2px",
+            paddingRight: "2px",
+          }}
+        >
+          <List>
+            <ListItem key="typoTitle">
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ width: "100%", maxWidth: "800px" }}
               >
-                {contentsCodeList.map((item, index) => {
-                  return (
-                    <MenuItem key={item.code} value={item.code}>
-                      <Typography variant="body2" color="text.primary">
-                        {item.codeName}
-                      </Typography>
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="난이도"
-                onChange={(evt) => {
-                  setDifficultyCode(evt.target.value);
-                }}
-                value={difficultyCode}
-                sx={{ width: "50%" }}
-              >
-                {difficultyCodeList.map((item, index) => {
-                  return (
-                    <MenuItem key={item.code} value={item.code}>
-                      {item.codeName}
-                    </MenuItem>
-                  );
-                })}
-              </Select>
-            </Stack>
-          </ListItem>
-          <Divider />
-          {/* 날짜 및 시간 선택 시작 */}
-          <ListItem>
-            <Typography variant="body2" color="text.secondary" sx={{ width: "100%" }}>
-              {!isUnknown ? "날짜 및 시간 선택" : "날짜미정의 경우 비고를 입력하세요."}
-            </Typography>
-            <FormControlLabel
-              control={
-                <Switch
-                  checked={isUnknown}
-                  onChange={(evt) => {
-                    setIsUnknown(evt.target.checked);
-                  }}
-                />
-              }
-              label="날짜미정"
-              sx={{ alignSelf: "flex-start", margin: "10px 0px 0px 0px" }}
-            />
-          </ListItem>
-          {/* 날짜미정 false일 경우 일정 선택 */}
-          {!isUnknown && (
-            <ListItem>
-              <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ko">
-                <DemoContainer components={["DateTimePicker"]}>
-                  <DateTimePicker
-                    label="Tip! 오전/오후를 알맞게 선택하세요"
-                    ampm={true}
-                    id="dateTimePicker"
-                    value={dayjs(datePickerValue)}
-                    onChange={(val) => {
-                      setDatePickerValue(dayjs(val).format(`YYYY-MM-DD HH:mm`));
-                      console.log(datePickerValue);
-                    }}
-                  />
-                </DemoContainer>
-              </LocalizationProvider>
+                파티제목
+              </Typography>
             </ListItem>
-          )}
-          {/* 날짜미정 true 경우 비고란 입력 */}
-          {isUnknown && (
-            <ListItem>
+            <ListItem key="inputTitle">
               <TextField
-                id="outlined-required"
-                label="ex)이번주말, 태신과 협의"
-                value={unknownRemark}
-                inputProps={{ maxLength: 25 }}
+                id="title"
+                label="제목"
+                variant="outlined"
+                value={title}
+                autoFocus
                 onChange={(evt) => {
-                  if (evt.target.value.length > 25) {
-                    alert("25글자 까지만 입력 가능합니다.");
-                    return false;
+                  if (evt.target.value.length > 39) {
+                    alert("40자까지만 입력하세요.");
                   }
-                  setUnknownRemark(evt.target.value);
+                  setTitle(evt.target.value);
                 }}
-                sx={{ width: "200px", marginTop: "10px" }}
               />
             </ListItem>
-          )}
-          {/* 날짜 및 시간 선택 끝 */}
-          <Divider />
-          {/* 최대인원 시작 */}
-          <ListItem key="typoLimitMember">
-            <Typography variant="body2" color="text.secondary" sx={{ width: "100%" }}>
-              최대인원
-            </Typography>
-          </ListItem>
-          <ListItem key="inputLimitMember">
-            <TextField
-              id="title"
-              label="최대인원"
-              variant="outlined"
-              type="number"
-              value={limitMember}
-              onChange={(evt) => {
-                if (evt.target.value > 99) {
-                  alert("99 까지만 입력 가능합니다.");
-                  return false;
+            <Divider />
+            <ListItem key="typoContentAndDifficulty">
+              <Typography variant="body2" color="text.secondary">
+                컨텐츠 및 난이도 선택
+              </Typography>
+            </ListItem>
+            <ListItem key="selectContentAndDifficulty">
+              <Stack
+                direction="row"
+                justifyContent="start"
+                alignItems="start"
+                spacing={0}
+                sx={{ width: "100%" }}
+              >
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  label="컨텐츠"
+                  onChange={(evt) => {
+                    setContentsCode(evt.target.value);
+                  }}
+                  value={contentsCode}
+                  sx={{ width: "50%" }}
+                >
+                  {contentsCodeList.map((item, index) => {
+                    return (
+                      <MenuItem key={item.code} value={item.code}>
+                        <Typography variant="body2" color="text.primary">
+                          {item.codeName}
+                        </Typography>
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  label="난이도"
+                  onChange={(evt) => {
+                    setDifficultyCode(evt.target.value);
+                  }}
+                  value={difficultyCode}
+                  sx={{ width: "50%" }}
+                >
+                  {difficultyCodeList.map((item, index) => {
+                    return (
+                      <MenuItem key={item.code} value={item.code}>
+                        {item.codeName}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+              </Stack>
+            </ListItem>
+            <Divider />
+            {/* 날짜 및 시간 선택 시작 */}
+            <ListItem>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ width: "100%" }}
+              >
+                {!isUnknown
+                  ? "날짜 및 시간 선택"
+                  : "날짜미정의 경우 비고를 입력하세요."}
+              </Typography>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={isUnknown}
+                    onChange={(evt) => {
+                      setIsUnknown(evt.target.checked);
+                    }}
+                  />
                 }
-                setLimitMember(evt.target.value);
-              }}
-            />
-            
-          </ListItem>
-          {/* 최대인원 종료 */}
-          <Divider />
-          <ListItem key='saveBtn' sx={{justifyContent:'end'}}>
-          <Button  variant="contained" color="primary" onClick={handleSaveClick}>
-              저장
-            </Button>
-          </ListItem>
-        </List>
+                label="날짜미정"
+                sx={{ alignSelf: "flex-start", margin: "10px 0px 0px 0px" }}
+              />
+            </ListItem>
+            {/* 날짜미정 false일 경우 일정 선택 */}
+            {!isUnknown && (
+              <ListItem>
+                <LocalizationProvider
+                  dateAdapter={AdapterDayjs}
+                  adapterLocale="ko"
+                >
+                  <DemoContainer components={["DateTimePicker"]}>
+                    <DateTimePicker
+                      label="Tip! 오전/오후를 알맞게 선택하세요"
+                      ampm={true}
+                      id="dateTimePicker"
+                      value={dayjs(datePickerValue)}
+                      onError={() => {
+                        console.log("error!");
+                      }}
+                      slotProps={{ textField: { readOnly: true } }}
+                      minutesStep={5}
+                      onChange={(val) => {
+                        console.log("val : ", val);
+                        setDatePickerValue(
+                          dayjs(val).format(`YYYY-MM-DD HH:mm`)
+                        );
+                      }}
+                    />
+                  </DemoContainer>
+                </LocalizationProvider>
+              </ListItem>
+            )}
+            {/* 날짜미정 true 경우 비고란 입력 */}
+            {isUnknown && (
+              <ListItem>
+                <TextField
+                  id="outlined-required"
+                  label="ex)이번주말, 태신과 협의"
+                  value={unknownRemark}
+                  inputProps={{ maxLength: 25 }}
+                  onChange={(evt) => {
+                    if (evt.target.value.length > 25) {
+                      alert("25글자 까지만 입력 가능합니다.");
+                      return false;
+                    }
+                    setUnknownRemark(evt.target.value);
+                  }}
+                  sx={{ width: "200px", marginTop: "10px" }}
+                />
+              </ListItem>
+            )}
+            {/* 날짜 및 시간 선택 끝 */}
+            <Divider />
+            {/* 최대인원 시작 */}
+            <ListItem key="typoLimitMember">
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                sx={{ width: "100%" }}
+              >
+                최대인원
+              </Typography>
+            </ListItem>
+            <ListItem key="inputLimitMember">
+              <TextField
+                id="title"
+                label="최대인원"
+                variant="outlined"
+                type="number"
+                value={limitMember}
+                onChange={(evt) => {
+                  if (evt.target.value > 99) {
+                    alert("99 까지만 입력 가능합니다.");
+                    return false;
+                  }
+                  setLimitMember(evt.target.value);
+                }}
+              />
+            </ListItem>
+            {/* 최대인원 종료 */}
+            <Divider />
+            <ListItem key="saveBtn" sx={{ justifyContent: "end" }}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSaveClick}
+              >
+                저장
+              </Button>
+            </ListItem>
+          </List>
         </Container>
       </Dialog>
     </div>
